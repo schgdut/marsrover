e4fdd4cde695a45b8b4ba58291b97d36310c5d42
孙晨皓 <sun@haoge.local>
2020-06-19 19:18:38 +0800

    sunchenhao

--- src/test/java/com/twschool/practice/GuessNumberGameTest.java
+++ src/test/java/com/twschool/practice/GuessNumberGameTest.java
@@ -25,6 +25,16 @@ public class GuessNumberGameTest {
         Assert.assertEquals(GameStatus.SUCCEED,gameStatus);
     }
 
+    @Test
+    public void should_return_contitnue_when_get_status_after_input_1256_given_game_with_answer_1234() {
+        GameAnswer gameAnswer = new GameAnswer("1 2 3 4");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(gameAnswer);
+        guessNumberGame.guess("1 2 5 6");
+
+        GameStatus gameStatus = guessNumberGame.getStatus();
+
+        Assert.assertEquals(GameStatus.CONTITNUE,gameStatus);
+    }
 
 }
 
