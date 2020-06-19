4e6faa34db2173d92f719d89b0551a18a493bc71
孙晨皓 <sun@haoge.local>
2020-06-19 17:32:25 +0800

    sunchenhao

--- src/test/java/com/twschool/practice/AnswerTest.java
+++ src/test/java/com/twschool/practice/AnswerTest.java
@@ -94,4 +94,13 @@ public class AnswerTest {
         Assert.assertEquals("1A0B",result);
     }
 
+    @Test
+    public void should_return_0A3B_when_compare_with_1234_given_answer_4325() {
+        GameAnswer answer = new GameAnswer("1 2 3 4");
+
+        String result = answer.check("4 3 2 5");
+
+        Assert.assertEquals("0A3B",result);
+    }
+
 }
