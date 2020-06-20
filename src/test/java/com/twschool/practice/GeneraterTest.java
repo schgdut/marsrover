8d5605c9bb68a77d586dbb51343840624e738ff5
孙晨皓 <sun@haoge.local>
2020-06-20 13:00:12 +0800

    sunchenhao

---
+++ src/test/java/com/twschool/practice/GeneraterTest.java
@@ -0,0 +1,13 @@
+package com.twschool.practice;
+
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+public class GeneraterTest {
+    @Test
+    public static boolean isInteger(String str) {
+        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+        return pattern.matcher(str).matches();
+    }
+}
